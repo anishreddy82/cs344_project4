df9f71135d66540a646e45b39ec534e7b0fa5d36
Anish Reddy <50680828+anishreddy82@users.noreply.github.com>
2020-08-11 10:08:23 -0700

    Update multi.c

--- multi.c
+++ multi.c
@@ -14,22 +14,25 @@
 char buffer[SIZE];
 char buffer1[SIZE];
 char buffer2[SIZE];
+char buffer3[SIZE];
 // Number of items in the buffer, shared resource
 int count = 0;
+int count1 = 0;
+int count2 = 0;
 // Index where the producer will put the next item
 int prod_idx = 0;
 // Index where the consumer will pick up the next item
 int con_idx = 0;
 // How many items will be produced
-int num_iterations = 0;
+int num_iterations = 10;
 
 // Initialize the mutex
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 // Initialize the condition variables
 pthread_cond_t cv_buffer1 = PTHREAD_COND_INITIALIZER;
-pthread_cond_t full = PTHREAD_COND_INITIALIZER;
-pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cv_buffer2 = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cv_buffer3 = PTHREAD_COND_INITIALIZER;
 
 /*
  Produce an item. Produces a random integer between [0, 1000] unless it is the last item to be produced in which case the value -1 is returned.
@@ -39,7 +42,10 @@ char* produce()
     char *line = NULL;
     size_t size;
     if(getline(&line, &size, stdin) == -1){
-        printf("No new line\n");
+        exit(0);
+    }
+    else if(strcmp(line, "DONE\n") == 0){
+        strcpy(buffer, line);
     }
     else{
         strcpy(buffer, line);
@@ -55,11 +61,15 @@ char* produce()
 */
 void *producer(void *args)
 {
-      printf("PROD %s\n", produce());
-      // Signal to the consumer that the buffer is no longer empty
-      pthread_cond_signal(&cv_buffer1);
-      // Unlock the mutex
-      pthread_mutex_unlock(&mutex);
+        pthread_mutex_lock(&mutex);
+        while(count == 1){
+            pthread_cond_wait(&cv_buffer1, &mutex);
+        }
+        produce();
+        // Signal to the consumer that the buffer is no longer empty
+        pthread_cond_signal(&cv_buffer1);
+        // Unlock the mutex
+        pthread_mutex_unlock(&mutex);
     return NULL;
 }
 
@@ -77,6 +87,7 @@ char *consume()
         }
     }
     count = 0;
+    count1 = 1;
     return buffer1;
 }
 
@@ -85,39 +96,74 @@ char *consume()
 */
 void *consumer(void *args)
 {
-      // Lock the mutex before checking if the buffer has data      
+      // Lock the mutex before checking if the buffer has data  
       pthread_mutex_lock(&mutex);
-      printf("CONS %s\n", consume());
+      while(count == 0){
+        pthread_cond_wait(&cv_buffer1, &mutex);
+      }
+      consume();
       // Signal to the producer that the buffer has space
-      pthread_cond_signal(&cv_buffer1);
       pthread_cond_signal(&cv_buffer2);
+      pthread_cond_signal(&cv_buffer1);
       // Unlock the mutex
       pthread_mutex_unlock(&mutex);
     return NULL;
 }
 
 
-char* consumer1(){
-    char replace[] = "^";
-    char toReplace[] = "++";
-    int i;
-    int size = sizeof(buffer);
-    memcpy(&buffer2, &buffer1, size);
-    for(i = 0; buffer2[i] != '\0'; i++){
-        if(strstr(&buffer2[i], toReplace) == )
+char* consume1(){
+
+    int size = sizeof(buffer1);
+    int i, j;
+    if(buffer1 == NULL){
+        return;
     }
+    for(i = 1; buffer1[i] != '\0'; i++){
+        if (buffer1[i-1]=='+' && buffer1[i]=='+'){
+            buffer1[i-1] = '^'; 
+            for (j=i; buffer1[j]!='\0'; j++) 
+                buffer1[j] = buffer1[j+1]; 
+        }
+    }
+    memcpy(&buffer2, &buffer1, size);
+    count1 = 0;
+    count2 = 1;
+    return buffer2;
 }
 
 void *consumer1(void *args){
       // Lock the mutex before checking if the buffer has data      
       pthread_mutex_lock(&mutex);
-
-      printf("CONS %d\n", consume1());
+      while(count1 == 0){
+          pthread_cond_wait(&cv_buffer2, &mutex);
+      }
+      consume1();
       // Signal to the producer that the buffer has space
       pthread_cond_signal(&cv_buffer2);
+      pthread_cond_signal(&cv_buffer3);
       // Unlock the mutex
       pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+char* consume2(){
+    int counter = 0;
+    int i;
+    int size = sizeof(buffer2[0]) * 80;
+    memcpy(&buffer3, &buffer2, size);
+    count2 = 0;
+    return buffer3;
+}
+
+void *consumer2(void *args){
+    pthread_mutex_lock(&mutex);
+    while(count2 == 0){
+        pthread_cond_wait(&cv_buffer3, &mutex);
     }
+    consume2();
+    printf("%s\n", buffer3);
+    pthread_cond_signal(&cv_buffer3);
+    pthread_mutex_unlock(&mutex);
     return NULL;
 }
 
@@ -125,7 +171,7 @@ int main(int argc, char *argv[])
 {
 
     // Create the producer and consumer threads
-    pthread_t p, c, p1, c2;
+    pthread_t p, c, c1, c2;
     pthread_create(&p, NULL, producer, NULL);
     // Sleep for a few seconds to allow the producer to fill up the buffer. This has been put in to demonstrate the the producer blocks when the buffer is full. Real-world systems won't have this sleep  
     sleep(5);  
@@ -133,8 +179,12 @@ int main(int argc, char *argv[])
 
     sleep(5);
     pthread_create(&c1, NULL, consumer1, NULL);
+    
+    sleep(5);
+    pthread_create(&c2, NULL, consumer2, NULL);
     pthread_join(p, NULL);
     pthread_join(c, NULL);
     pthread_join(c1, NULL);
+    pthread_join(c2, NULL);
     return 0;
 }
